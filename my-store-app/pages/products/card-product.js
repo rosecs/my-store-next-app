d2721533db51e9f233e27354ed061ae95f9d7bd7
rosecs <32778331+rosecs@users.noreply.github.com>
2022-05-26 13:08:05 -0500

    New: create next.js project and add the card product template

---
+++ my-store-app/pages/products/card-product.js
@@ -0,0 +1,13 @@
+export default function CardProduct() {
+    return (
+        <>
+            <div id="headerProducts">
+                <img src="https://i.pinimg.com/236x/81/e7/8b/81e78b410ad30166bc8a384bc67b393d.jpg" />
+                <p> Bolsa </p>
+                <p> $200 USD</p>
+                <button> add cart</button>
+                <button> Read more </button>
+                </div>
+        </>
+    );
+}
